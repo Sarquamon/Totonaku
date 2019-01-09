9d5b053481927a810d210daabc494d67b5c0ce4d
Salomon <sarquamon@gmail.com>
2019-01-09 08:03:53 -0600

    Se añadió numeros1Level2_2

---
+++ app/src/main/java/com/example/salo7/totonaku/numeros1Level2_2.java
@@ -0,0 +1,13 @@
+package com.example.salo7.totonaku;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class numeros1Level2_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numeros1_level2_2);
+    }
+}
